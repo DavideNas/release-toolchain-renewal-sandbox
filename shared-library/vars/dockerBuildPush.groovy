5464ad6d1619ef008b58b11aa024b839bc187b18
DavideNas <d.naselli@gmail.com>
2026-06-27 15:35:47 +0200

    fix docker path to pull pom.xml

--- shared-library/vars/dockerBuildPush.groovy
+++ shared-library/vars/dockerBuildPush.groovy
@@ -1,21 +1,18 @@
 def call(String imageName, Object imageTag) {
 
-    // Convertiamo esplicitamente il tag in Stringa pura, così se arriva un GString da Jenkins viene ripulito
+    // Convertiamo esplicitamente il tag in Stringa pura
     def tag = imageTag.toString()
 
     if(!imageName) {
         error " ❌ [Shared Library] Il parametro 'imageName' è obbligatorio"
     }
 
-    // Recuperiamo il percorso del client Docker installato tramite i Tools di Jenkins
-    def dockerTool = tool name: 'docker-tool', type: 'org.jenkinsci.plugins.docker.commons.tools.DockerTool'
+    echo "🐳 [Shared Library] Costruzione immagine Docker: ${imageName}:${tag}..."
 
-    // Costruiamo il percorso completo del binario
-    def dockerBinary = "${dockerTool}/bin/docker"
-
-    echo "🐳 [Shared Library] Costruzione immagine Docker: ${imageName}:${imageTag}..."
-
-    // Utilizziamo il percorso assoluto del tool per eseguire il comando build
-    // Aggiungiamo il percorso (il punto alla fine indica il contesto, -f indica dove sta il Dockerfile)
-    sh "docker build --no-cache -t ${imageName}:${tag} -f services/order-service/Dockerfile ."
+    // Utilizziamo il comando 'docker' nativo (ora installato nel container)
+    // Usiamo 'dir' per spostarci nella cartella corretta prima di eseguire il build.
+    // In questo modo il contesto di build è corretto e Docker trova pom.xml e src.
+    dir('services/order-service') {
+        sh "docker build --no-cache -t ${imageName}:${tag} ."
+    }
 }
